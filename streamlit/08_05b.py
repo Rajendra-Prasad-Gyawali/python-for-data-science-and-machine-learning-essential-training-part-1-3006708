daadd5f91ee8eee4a708bd333e68415d7cff1dde
Rajendra-Prasad-Gyawali <53804987+Rajendra-Prasad-Gyawali@users.noreply.github.com>
2025-04-21 07:12:40 +0000

    Completed lesson 08_05 (bar chart and pie charts in streamlit)

--- streamlit/08_05b.py
+++ streamlit/08_05b.py
@@ -1,1 +1,29 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
 
+animals = ['cat', 'cow', 'goat', 'dog']
+heights = [30,60,40,35]
+weights = [5,400,80,60]
+
+fig, ax = plt.subplots()
+
+x = np.arange(len(heights))
+width = 0.40
+
+ax.bar(x-0.2, heights, width, color='red')
+ax.bar(x+0.2, weights, width, color='orange')
+
+ax.legend(['height', 'weight'])
+ax.set_xticks(x)
+ax.set_xticklabels(animals)
+
+st.pyplot(fig)
+
+explode = [0.2, 0.1, 0.1, 0.1]
+plot_pie, ax = plt.subplots()
+ax.pie(heights,explode=explode, labels=animals, autopct='%1.1f%%', shadow=True)
+ax.axis('equal')
+
+st.pyplot(plot_pie)
